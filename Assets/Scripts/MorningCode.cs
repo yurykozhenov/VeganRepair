58a9d3d698ad3ac445451c3a7a828a4981859547
EndL11 <47588502+EndL11@users.noreply.github.com>
2020-02-01 15:56:05 +0200

    pushinka

---
+++ Assets/Scripts/MorningCode.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MorningCode : MonoBehaviour
+{
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    //else if(collision.tag == "BodyPoint")
+    //    //{
+    //    //    if (onPoint || collision.GetComponent<BodyPoint>().isTriggered)
+    //    //    {
+    //    //        return;
+    //    //    }
+    //    //    onPoint = true;
+    //    //    point = collision.transform;
+    //    //    collision.GetComponent<BodyPoint>().Triggered();
+    //    //    moving = false;
+    //    //}
+    //    //else if (collision.tag == "Body")
+    //    //{
+    //    //    if (onPoint)
+    //    //    {
+    //    //        return;
+    //    //    }
+    //    //    onPoint = true;
+    //    //    point = collision.transform;
+    //    //    moving = false;
+    //    //}
+    //}
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    //if (collision.gameObject.tag == "Body" && collision.gameObject.GetComponent<ElementLogic>().onPoint && !collision.gameObject.GetComponent<ElementLogic>())
+    //    //{
+    //    //    if (onPoint)
+    //    //    {
+    //    //        return;
+    //    //    }
+    //    //    onPoint = true;
+    //    //    moving = false;
+    //    //    Vector2 hitPoint = collision.GetContact(0).point;
+    //    //    point = hitPoint;
+    //    //    body = collision.gameObject;
+    //    //    Debug.Log(point);
+    //    //    transform.position = hitPoint;
+    //    //    GetComponent<Rigidbody2D>().gravityScale = 0f;
+    //    //    GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+    //    //}
+    //}
+}
