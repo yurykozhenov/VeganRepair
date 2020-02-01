6aa44cdeb2c580469f4d05164617ffb094947bea
EndL11 <47588502+EndL11@users.noreply.github.com>
2020-02-01 23:23:56 +0200

    canvas score

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,20 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
-    public GameObject body;
-    public float rotatingSpeed = 30f;
+    public static LevelManager instance;
+    public int mark;
+    public int score;
+    public Text scoreText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //body.transform.Rotate(Time.deltaTime * rotatingSpeed * Vector3.forward);
+        scoreText.text = score.ToString();
     }
 }
