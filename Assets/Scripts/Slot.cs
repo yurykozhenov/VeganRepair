8d01a7de6348977b09f2c553cfca86370af07c44
EndL11 <47588502+EndL11@users.noreply.github.com>
2020-02-02 04:36:11 +0200

    correct hands

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -6,6 +6,8 @@ public class Slot : MonoBehaviour
 {
     public string bodyName;
     public bool connected = false;
+    public GameObject body;
+    private bool moving = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,17 @@ public class Slot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (connected && moving)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, body.transform.position, 1f * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == body.gameObject)
+        {
+            moving = false;
+        }
     }
 }
